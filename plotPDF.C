e6c3005028b8b3d2c7b3b5c3a260dd158594391f
yjlee <yenjie.lee@cern.ch>
2016-05-30 08:56:10 -0400

    update

--- plotPDF.C
+++ plotPDF.C
@@ -1,7 +1,12 @@
+#include <vector>
+#include <iostream>
+
 #include <TFile.h>
 #include <TCanvas.h>
 #include <TH1D.h>
-#include <vector>
+#include <TString.h>
+#include <TLine.h>
+#include <TBox.h>
 
 void normalize(TH1D *h)
 {
@@ -30,28 +35,93 @@ void normalize(TH1D *h)
   KEY: TH1D	histerr_ptave150_400;1	
 */
 
-void plot(int ptMin=25,int ptMax=55)
+void plotPDF(int ptMin=25,int ptMax=55, bool isPPb=1, int pdfSet=2)
 {
-   TCanvas *c = new TCanvas("c","",600,600);
-   TH1D *hCentral;
    
-   // 0 is the central
+
+
+   TCanvas *c = new TCanvas("c","",600,600);
+   
+   // 00 is the central
    
    vector <TH1D*> hList;
    
-   for (int i=0;i<=50;i++)
+   string fileName;
+   int nEigenvalues=0;
+   
+   if (isPPb && pdfSet==1) {
+      // DSSZ 
+      fileName = "parsed_pPbNLO/PPBCMSNEWMMD";
+      nEigenvalues = 25;
+   } else if (isPPb && pdfSet==2) {
+      // EPS09
+      cout <<"EPS09"<<endl;
+      fileName = "parsed_pPbNLO/PPBCMSNEWMME";
+      nEigenvalues = 15;
+   }
+   
+   TFile *infData = new TFile("../outf_pPb_pp.root");
+   int idx=0;
+   if (ptMin==25) idx=0;
+   if (ptMin==55) idx=1;
+   if (ptMin==75) idx=2;
+   if (ptMin==95) idx=3;
+   if (ptMin==115) idx=4;
+   if (ptMin==150) idx=5;
+   
+   TH1D *hData = (TH1D*)infData->Get(Form("hist_eta_pPb_%d",idx));
+
+   hData->Draw();   
+   // read files
+   for (int i=0;i<=2*nEigenvalues;i++)
    {
       TFile *inf;
-      if (i<10) inf = new TFile(Form("PPBCMSNEWCTD0%d.out.root",i));
-      else inf = new TFile(Form("PPBCMSNEWCTD%d.out.root",i));
+
+      if (i<10) inf = new TFile(Form("%s0%d.out.root",fileName.c_str(),i));
+      else inf = new TFile(Form("%s%d.out.root",fileName.c_str(),i));
+
       if (inf->IsZombie()) continue;
       TH1D *h = (TH1D*)inf->Get(Form("hist_ptave%d_%d",ptMin,ptMax));
       h->SetName(Form("hist_ptave25_55_%2d",i));
       normalize(h);
-      if (i==0) hCentral=h;
+      
+      
       h->SetLineColor(kGray);
-      if(i!=0) h->Draw("same"); else h->Draw();      
+      if(i!=0) h->Draw("same"); //else h->Draw();      
+      hList.push_back(h);
    }
-   hCentral->SetLineColor(2);
-   hCentral->Draw("same");
+   
+   
+   for (int j=1;j<=hList[0]->GetNbinsX();j++) 
+   {
+      double deltaXp=0;
+      double deltaXn=0;
+      for (int i=1;i<=nEigenvalues;i++)
+      {
+         double diff1 = hList[i*2-1]->GetBinContent(j)-hList[0]->GetBinContent(j);
+	 double diff2 = hList[i*2]->GetBinContent(j)-hList[0]->GetBinContent(j);
+	 double diffp=0;
+	 double diffn=0;
+	 if (diff1>diffp) diffp=diff1;
+	 if (diff2>diffp) diffp=diff2;
+	 if (-diff1>diffn) diffn=-diff1;
+	 if (-diff2>diffn) diffn=-diff2;
+	 deltaXp+=diffp*diffp;
+	 deltaXn+=diffn*diffn;
+      }
+      
+      deltaXp=sqrt(deltaXp);
+      deltaXn=sqrt(deltaXn);
+      
+      TBox *l = new TBox(hList[0]->GetBinLowEdge(j),hList[0]->GetBinContent(j)+deltaXp,hList[0]->GetBinLowEdge(j+1),hList[0]->GetBinContent(j)-deltaXn);
+      l->SetFillStyle(0);
+      l->SetLineColor(4);
+      l->Draw();
+      
+   }
+   
+   hList[0]->SetLineColor(2);
+   hList[0]->Draw("same");
+   hData->Draw("same");
+
 }
